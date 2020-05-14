2294e84c52e8d5cdd0122f00b6975a294af6b88f
HanM.G <hanmoongoo@gmail.com>
2020-05-14 22:22:44 +0900

    post 요청처리와 view engine (ejs) 사용, JSON 처리방법등

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var app = express()
+var bodyParser = require('body-parser')
 
 // 동기가 다 되고 나서 비동기가 시작된다.
 app.listen(3000, function(){
@@ -14,6 +15,13 @@ console.log('end of server code...')
 // static소스 위치 설정
 app.use(express.static('public'))
 
+// bodyParser 설정 ( json형태, 인코딩되서온 형태 처리 )
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+
+// view engine (template 설정)
+app.set('view engine', 'ejs')
+
 // url routing
 app.get('/', function(req,res){
     // __dirname하면 절대경로를 알아서 표현해준다고함
@@ -23,3 +31,24 @@ app.get('/', function(req,res){
 app.get('/main', function(req,res){
     res.sendFile(__dirname + "/public/main.html")
 })
+
+app.post('/email_post',function(req,res){
+    // 오브젝트 형태
+    //console.log(req.body)
+
+    // 이메일 정보
+    //console.log(req.body.email)
+
+    //res.send("<h1>welcome!</h1>" + req.body.email)
+    // email.ejs를 찾아서 해당 데이터를 보내줌
+    res.render('email.ejs',{'email' : req.body.email})
+
+});
+
+// ajax
+app.post('/ajax_send_email',function(req,res){
+    console.log(req.body.email)
+    // 원래는 뒤에 처리전에 입력받은 값에 대한 체크를 해야함 => select or insert from DB
+    var responseData = {'result': 'ok', 'email': req.body.email}
+    res.json(responseData);
+});
